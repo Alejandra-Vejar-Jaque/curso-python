8ad5fc5a7633da414008082d890ca86a62616461
Alejandra Vejar <alejandravejarjaque1975>
2024-10-12 17:47:28 -0300

    Agregar otro celular

--- Python/Python POO/Clases y Objetos.py
+++ Python/Python POO/Clases y Objetos.py
@@ -9,6 +9,7 @@ class Celular:
 celular1 = Celular("Samsung","S23","48mp")
 celular2 = Celular("Apple","Sq","98mp")
 celular3 = Celular("Xiomi","hp","mp")
+celular4 = Celular("sony","hp","mp")
 
 
 print(celular1.marca)
